7e4ca461c4abfbfb49dfa2be4073f7d56b8a2edc
Nike Shian Guray <nikkiguray12@gmail.com>
2022-12-26 21:17:00 +0800

    css added

---
+++ config.js
@@ -0,0 +1,8 @@
+const constants = {
+    openWeatherMap: {
+        BASE_URL: "https://api.openweathermap.org/data/2.5/weather?q=",
+        SECRET_KEY: "07757c8cb4cb2f661f653c1ac0e3f71c"
+    }
+}
+
+module.exports = constants;
